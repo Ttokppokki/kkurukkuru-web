b832bf6e80614a5798b5ff79c666dfbdbf0da092
rkdalswl718 <rkdalswl070718@gmail.com>
2024-12-21 22:17:39 +0900

    add :: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Back from '../../public/icon/Back'
+
+const Header = () => {
+  return (
+    <div className='fixed flex h-10 p-6 items-start justify-center'>
+      <Back />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/letter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const letter = () => {
+  return (
+    <div className='flex flex-col min-h-screen bg-white relative overflow-x-hidden'>
+      
+      
+    </div>
+  );
+};
+
+export default letter;
